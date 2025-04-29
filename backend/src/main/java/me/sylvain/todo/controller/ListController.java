ddff3cc48af6267d7113fe9c7bade667d5463cbd
Sylvain Delarche <sylvain.delarche@gmail.com>
2025-04-29 19:58:25 +0200

    Added ListController for list API

--- backend/src/main/java/me/sylvain/todo/controller/ListController.java
+++ backend/src/main/java/me/sylvain/todo/controller/ListController.java
@@ -1,6 +1,13 @@
 package me.sylvain.todo.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +30,37 @@ public class ListController {
     @PostMapping
     public ResponseEntity<TodoList> createList(@Valid @RequestBody TodoListDTO listDTO) {
         TodoList list = new TodoList();
-        list.setTitle(listDTO.getTitle());
-
-        return ResponseEntity.ok(listRepository.save(list));
+        if (listDTO.getTitle() != null) list.setTitle(listDTO.getTitle());
+        return ResponseEntity.status(HttpStatus.OK).body(listRepository.save(list));
+    }
+    
+    @PatchMapping("/{id}")
+    public ResponseEntity<TodoList> updateList(@PathVariable Long id, @Valid @RequestBody TodoListDTO listDTO) {
+        return listRepository.findById(id).map(list -> {
+            if (listDTO.getTitle() != null) list.setTitle(listDTO.getTitle());
+            return ResponseEntity.status(HttpStatus.OK).body(listRepository.save(list));
+        }).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteList(@PathVariable Long id) {
+        if (listRepository.findById(id).isPresent()) {
+            listRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TodoList>> getAllLists() {
+        return ResponseEntity.status(HttpStatus.OK).body(listRepository.findAll());
+    }
+    
+    @PostMapping("/{id}/archive")
+    public ResponseEntity<TodoList> archiveList(@PathVariable Long id) {
+        return listRepository.findById(id).map(list -> {
+            list.setActive(list.isActive() ? false : true);
+            return ResponseEntity.status(HttpStatus.OK).body(listRepository.save(list));
+        }).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
 }
