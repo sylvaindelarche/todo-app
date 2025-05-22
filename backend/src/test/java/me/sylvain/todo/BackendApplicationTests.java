6e8ae3858a7776bd4516024b08f1a1394ec73dc1
Sylvain Delarche <sylvain.delarche@gmail.com>
2025-05-22 22:53:47 +0200

    Fix initial tests in prep for CI/CD

--- backend/src/test/java/me/sylvain/todo/BackendApplicationTests.java
+++ backend/src/test/java/me/sylvain/todo/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package me.sylvain.todo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.datasource.url=jdbc:h2:mem:testdb")
 class BackendApplicationTests {
 
 	@Test
